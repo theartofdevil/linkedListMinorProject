b29848b22690e4d2e759b9171c08e1460983e723
theartofdevil <miracunlu98@gmail.com>
2020-12-17 22:01:16 +0300

    Initial Commit

---
+++ prolab proje 3/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  prolab proje 3
+//
+//  Created by Mr.Engineer on 2020-12-17.
+//  Copyright © 2020 Mr.Engineer. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
